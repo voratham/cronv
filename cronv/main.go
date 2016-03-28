bc4360b9717578358ffac8f63aa13b67ddf47096
takumakanari <chemtrails.t@gmail.com>
2016-03-28 20:25:58 +0900

    split option handlings to cmd module

--- cronv/main.go
+++ cronv/main.go
@@ -2,90 +2,37 @@ package main
 
 import (
 	"bufio"
-	"errors"
-	"flag"
 	"fmt"
+	"github.com/jessevdk/go-flags"
 	"github.com/takumakanari/cronv"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 )
-
-const VERSION = "0.2.0"
 
 const (
-	OPT_DATE_FORMAT         = "2006/01/02"
-	OPT_TIME_FORMAT         = "15:04"
-	OPT_OUTPUT_PATH_DEFAULT = "./crontab.html"
+	VERSION = "0.2.0"
+	NAME    = "Cronv"
 )
 
-func optimizeTime(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), 0, 0, time.UTC)
-}
-
-func durationToMinutes(s string) (float64, error) {
-	length := len(s)
-	if length < 2 {
-		return 0, errors.New(fmt.Sprintf("Invalid duration format: '%s'", s))
-	}
-
-	duration, err := strconv.Atoi(string(s[:length-1]))
-	if err != nil {
-		return 0, errors.New(fmt.Sprintf("Invalid duration format: '%s', %s", s, err))
-	}
-
-	unit := string(s[length-1])
-	switch strings.ToLower(unit) {
-	case "d":
-		return float64(duration * 24 * 60), nil
-	case "h":
-		return float64(duration * 60), nil
-	case "m":
-		return float64(duration), nil
-	}
-
-	return 0, errors.New(fmt.Sprintf("Invalid duration format: '%s', '%s' is not in d/h/m", s, unit))
-}
-
-func toFromTime(d string, t string) (time.Time, error) {
-	return time.Parse(fmt.Sprintf("%s %s", OPT_DATE_FORMAT, OPT_TIME_FORMAT),
-		fmt.Sprintf("%s %s", d, t))
-}
-
 func main() {
-	var (
-		outputFilePath string
-		duration       string
-		fromDate       string
-		fromTime       string
-	)
-	now := optimizeTime(time.Now())
+	opts := cronv.NewCronvCommand()
 
-	for _, f := range []string{"o", "output"} {
-		flag.StringVar(&outputFilePath, f, OPT_OUTPUT_PATH_DEFAULT, "path to .html file to output.")
+	parser := flags.NewParser(opts, flags.Default)
+	parser.Name = fmt.Sprintf("%s v%s", NAME, VERSION)
+	if _, err := parser.Parse(); err != nil {
+		os.Exit(1)
 	}
-	for _, f := range []string{"d", "duration"} {
-		flag.StringVar(&duration, f, "6h",
-			"duration to visualize in N{suffix} style. e.g.) 1d(day)/1h(hour)/1m(minute).")
-	}
-	flag.StringVar(&fromDate, "from-date", now.Format(OPT_DATE_FORMAT),
-		fmt.Sprintf("start date in the format '%s' to visualize.", OPT_DATE_FORMAT))
-	flag.StringVar(&fromTime, "from-time", now.Format(OPT_TIME_FORMAT),
-		fmt.Sprintf("start time in the format '%s' to visualize.", OPT_TIME_FORMAT))
-	flag.Parse()
 
-	timeFrom, err := toFromTime(fromDate, fromTime)
+	timeFrom, err := opts.ToFromTime()
 	if err != nil {
 		panic(err)
 	}
 
-	durationMinutes, err := durationToMinutes(duration)
+	durationMinutes, err := opts.ToDurationMinutes()
 	if err != nil {
 		panic(err)
 	}
 
-	output, err := os.Create(outputFilePath)
+	output, err := os.Create(opts.OutputFilePath)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to handle output file: %s", err))
 	}
@@ -110,8 +57,8 @@ func main() {
 	cronv.MakeTemplate().Execute(output, map[string]interface{}{
 		"CronEntries": cronEntries,
 		"TimeFrom":    timeFrom,
-		"Duration":    duration,
+		"Duration":    opts.Duration,
 	})
 
-	fmt.Printf("'%s' generated successfully.\n", outputFilePath)
+	fmt.Printf("'%s' generated successfully.\n", opts.OutputFilePath)
 }

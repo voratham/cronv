6a303db53193340f1a1031b8f571bf4c598a54c0
takumakanari <chemtrails.t@gmail.com>
2017-03-15 19:20:20 +0900

    modify var name/scope

--- command.go
+++ command.go
@@ -9,11 +9,11 @@ import (
 )
 
 const (
-	opt_date_format         = "2006/01/02"
-	opt_time_format         = "15:04"
-	opt_default_duration    = "6h"
-	opt_default_output_path = "./crontab.html"
-	opt_default_title       = "cron tasks"
+	optDateFormat        = "2006/01/02"
+	optTimeFormat        = "15:04"
+	optDefaultDuration   = "6h"
+	optDefaultOutputPath = "./crontab.html"
+	optDefaultTitle      = "cron tasks"
 )
 
 type Command struct {
@@ -25,7 +25,7 @@ type Command struct {
 }
 
 func (self *Command) ToFromTime() (time.Time, error) {
-	return time.Parse(fmt.Sprintf("%s %s", opt_date_format, opt_time_format),
+	return time.Parse(fmt.Sprintf("%s %s", optDateFormat, optTimeFormat),
 		fmt.Sprintf("%s %s", self.FromDate, self.FromTime))
 }
 
@@ -57,10 +57,10 @@ func NewCronvCommand() *Command {
 	t := time.Now()
 	now := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), 0, 0, time.UTC)
 	return &Command{
-		OutputFilePath: opt_default_output_path,
-		Duration:       opt_default_duration,
-		FromDate:       now.Format(opt_date_format),
-		FromTime:       now.Format(opt_time_format),
-		Title:          opt_default_title,
+		OutputFilePath: optDefaultOutputPath,
+		Duration:       optDefaultDuration,
+		FromDate:       now.Format(optDateFormat),
+		FromTime:       now.Format(optTimeFormat),
+		Title:          optDefaultTitle,
 	}
 }
